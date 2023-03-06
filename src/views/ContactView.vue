0f0c7f64d50498a3618cecb63f7c040c9bea38e0
Ahmed <102857181+Ahmed2681@users.noreply.github.com>
2023-03-07 01:41:56 +0300

    mmm

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -9,12 +9,39 @@ import FooterPage from '@/components/FooterPages.vue'
       <header-page />
     </header>
 
-    <main class="w-full bg-cyan-50">
-      <section>
-        <div class="pt-[15%]">
-          <img src="../img/gym-saloon.webp" width="300" alt="gym-saloon" />
-        </div>
+    <main class="w-full bg-cyan-50 flex justify-center items-center text-center pt-[15%] pb-[15%] flex-col lg:flex-row md:pr-0 md:pl-0 pr-[10%] pl-[10%]">
+      <section class="flex flex-row lg:w-[40%] w-[100%] justify-center lg:justify-start lg:mb-[0%] mb-[10%]">
+        <img src="../img/gym-saloon.webp" class="rounded-lg lg:min-w-[500px] md:min-w-[420px]" alt="gym-saloon" />
       </section>
+      <article
+        class="border-dashed border-[2px] lg:ml-[15%] m-0 lg:max-w-[380px] rounded-lg border-cyan-300 justify-end flex flex-col items-center p-[2%]"
+      >
+        <div>
+          <p class="text-lg font-[600]">Telefon</p>
+          <hr
+            class="border-[2px] border-b-transparent border-l-transparent border-r-transparent w-[90%] border-dotted border-green-500"
+          />
+          <a href="tel:+ 01010101">+ 01010101</a>
+        </div>
+        <div class="pt-[4%]">
+          <p class="text-lg font-[600]">E-Mail</p>
+          <hr
+            class="border-[2px] border-b-transparent border-l-transparent border-r-transparent w-[90%] border-dotted border-green-500"
+          />
+          <a href = "mailto: lorem@ipsum.com">lorem@ipsum.com</a>
+        </div>
+        <div class="pt-[4%]">
+          <p class="text-lg font-[600]">Adres</p>
+          <hr
+            class="border-[2px] border-b-transparent border-l-transparent border-r-transparent w-[90%] border-dotted border-green-500"
+          />
+          <a href="https://www.google.com/maps" target="_blank">
+            <address>
+              Lorem Mah. Ipsum Dolor Cad. Sit Amet Sokak No: 4 Consectetur/ADİPİSİCİNG
+            </address>
+          </a>
+        </div>
+      </article>
     </main>
 
     <footer>
