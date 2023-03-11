65f9019120018e9ea810136fbb7aec5c978281f5
Ahmed <102857181+Ahmed2681@users.noreply.github.com>
2023-03-11 23:55:37 +0300

    mmm

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -9,10 +9,20 @@ import FooterPage from '@/components/FooterPages.vue'
       <header-page />
     </header>
 
-    <main
-      class="flex w-full flex-col items-center justify-center bg-cyan-50 pt-[5%] pb-[5%] text-center"
-    >
-      <article class="mb-[4%] flex items-center justify-center text-center">
+    <main class="flex w-full flex-col items-center justify-center bg-cyan-50 text-center">
+      <nav class="mb-[5%] mt-[3%]">
+        <ul class="flex flex-row items-center justify-center text-center">
+          <div class="min-w-[30%] rounded-full pl-4 pr-4 pt-1 pb-1 hover:bg-brown hover:text-white">
+            <a href="#Naim"><li>Naim Süleymanoğlu</li></a>
+          </div>
+          <li class="mr-2 ml-2">|</li>
+          <div class="min-w-[30%] rounded-full pl-4 pr-4 pt-1 pb-1 hover:bg-brown hover:text-white">
+            <a href="#Ali"><li>Muhammed Ali</li></a>
+          </div>
+        </ul>
+      </nav>
+
+      <article id="Naim" class="mb-[4%] flex items-center justify-center text-center">
         <img src="../img/Naim.webp" class="w-[40%] rounded-3xl" alt="Naim-Suleymanoglu" />
       </article>
       <section class="mt-[3%] flex flex-row items-center justify-center text-center">
@@ -65,7 +75,7 @@ import FooterPage from '@/components/FooterPages.vue'
         class="mt-[10%] mb-[10%] w-[90%] border-[10px] border-dotted border-black border-b-transparent border-l-transparent border-r-transparent"
       />
 
-      <article class="mb-[4%] flex items-center justify-center text-center">
+      <article id="Ali" class="mb-[4%] flex items-center justify-center text-center">
         <img src="../img/Muhammed-Ali.jpeg" class="w-[40%] rounded-3xl" alt="Muhammed-Ali" />
       </article>
 
@@ -85,12 +95,11 @@ import FooterPage from '@/components/FooterPages.vue'
       <article
         class="mt-[4%] flex w-[60%] items-center justify-center text-center font-mono text-2xl antialiased"
       >
-        Muhammed Ali (doğum adıyla Cassius Marcellus Clay Jr.; d. 17 Ocak 1942, Louisville, Kentucky
-        – ö. 3 Haziran 2016, Phoenix, Arizona), Amerikalı profesyonel boksördür. Tüm zamanların en
-        iyi boksörü olarak kabul edilen Muhammed Ali, 20. yüzyılın en önemli spor figürlerinden
-        biridir. "The Greatest" (En Büyük) lakaplı Ali, kariyeri boyunca yaptığı 61 maçın yalnızca 5
-        tanesini kaybetmiştir.[2][3] 1999 yılında Sports Illustrated tarafından "Yüzyılın Sporcusu"
-        ve BBC tarafından "Yüzyılın Spor Kişiliği" seçilmiştir.
+        Muhammed Ali, Amerikalı profesyonel boksördür. Tüm zamanların en iyi boksörü olarak kabul
+        edilen Muhammed Ali, 20. yüzyılın en önemli spor figürlerinden biridir. "The Greatest" (En
+        Büyük) lakaplı Ali, kariyeri boyunca yaptığı 61 maçın yalnızca 5 tanesini kaybetmiştir. 1999
+        yılında Sports Illustrated tarafından "Yüzyılın Sporcusu" ve BBC tarafından "Yüzyılın Spor
+        Kişiliği" seçilmiştir.
       </article>
 
       <br />
@@ -98,24 +107,32 @@ import FooterPage from '@/components/FooterPages.vue'
       <article
         class="flex w-[60%] items-center justify-center text-center font-mono text-2xl antialiased"
       >
-        Haltere 1977'de, 10 yaşındayken başladı. 15 yaşındayken Brezilya'da düzenlenen Dünya Gençler
-        Halter Şampiyonası'nda iki altın madalya alarak şampiyon oldu. 16 yaşında rekor kırarak yine
-        şampiyon oldu. Böylece halter tarihinde en genç dünya rekortmeni unvanını aldı. Kariyeri
-        boyunca 3 altın olimpiyat madalyası, 7 dünya şampiyonluğu ve 6 Avrupa şampiyonluğu vardır.
-        Tam 46 kere dünya rekoru kırmıştır. 1984 yılında (16 yaşındayken), silkme kategorisinde
-        vücut ağırlığının üç katını kaldıran ikinci halterci olarak tarihe geçti. 1983-1986 arasında
-        gençlerde 13, büyüklerde 50 olmak üzere tam 63 rekor kırarken yine bu dönemde dünya ve
-        Avrupa şampiyonalarında 52, 56 ve 60 kilolarda şampiyonluklar yaşadı. 1984, 1985 ve 1986'da
-        dünyada yılın haltercisi seçildi. <br />
+        Clay -doğum adıyla Cassius Marcellus Clay Jr.- 12 yaşındayken, bir hırsızın bisikletini
+        çalmasının ardından öfkeye kapılması üzerine, bir Louisville polis memuru ve boks koçu olan
+        Joe E. Martin tarafından boksa yönlendirildi. Clay, o anki öfkeyle memura hırsızı
+        kırbaçlayacağını söyledi. Memur Martin, Clay'e önce boks yapmayı öğrenmesinin daha iyi
+        olacağını söyledi. Başlangıçta Clay, Martin'in teklifini kabul etmedi; ancak daha sonra
+        Tomorrow's Champions adlı yerel bir televizyon boks programında amatör boksörleri gördükten
+        sonra dövüşle ilgilenmeye başladı. <br />
         <br />
-        1984 Los Angeles Olimpiyatları'na Bulgaristan'ın da Sovyetlerle boykotta yer alması
-        nedeniyle katılamadı. Bu dönemde Bulgar Hükûmetinin Soya Dönüş Operasyonu kapsamında Türk
-        isimlerini yasaklaması nedeniyle adı Naum Şalamanov olarak değiştirildi. Bulgaristan'daki bu
-        baskılardan kurtulmak ve Türkiye adına müsabakalara katılmak için 1986'da Melbourne'de
-        düzenlenen Dünya Halter Şampiyonası'nda Türkiye büyükelçiliğine sığınarak Türkiye'ye iltica
-        etti. Türkiye'ye ilticasında ve getirilmesinde bizzat Turgut Özal devreye girdi. Naim
-        Süleymanoğlu 18 Kasım 2017 günü tedavi gördüğü hastanede 50 yaşında hayatını kaybetti.
+        Muhammed Ali, 1978'de boksu şampiyon olarak bıraktı. 1984'te parkinson hastalığına
+        yakalanmasına rağmen, bunu gizleyip büyük para karşılığı ile iki maç daha yapıp bunları
+        kaybetti. Profesyonel döneminde sadece 5 kez yenilen, Olimpiyat ve Dünya Şampiyonu olan
+        Muhammed Ali, 36 yaşına kadar bütün şampiyonlar için tek isim olmayı başardı ve 37'si nakavt
+        olmak üzere toplam 56 maç kazandı.
       </article>
+
+      <nav class="mb-[5%] mt-[10%]">
+        <ul class="flex flex-row items-center justify-center text-center">
+          <div class="min-w-[30%] rounded-full pl-4 pr-4 pt-1 pb-1 hover:bg-brown hover:text-white">
+            <a href="#Naim"><li>Naim Süleymanoğlu</li></a>
+          </div>
+          <li class="mr-2 ml-2">|</li>
+          <div class="min-w-[30%] rounded-full pl-4 pr-4 pt-1 pb-1 hover:bg-brown hover:text-white">
+            <a href="#Ali"><li>Muhammed Ali</li></a>
+          </div>
+        </ul>
+      </nav>
     </main>
 
     <footer>
