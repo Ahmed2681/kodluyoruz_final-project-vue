9f14118fdd71ec32173c7949a410278c062633da
Ahmed <102857181+Ahmed2681@users.noreply.github.com>
2023-03-11 23:11:27 +0300

    mmm

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -12,9 +12,10 @@ import FooterPage from '@/components/FooterPages.vue'
     <main class="w-full bg-cyan-50 pt-[5%] pb-[5%]">
       <section class="flex flex-row items-center justify-center text-center">
         <p class="text-[60px] font-[550]">&lt;</p>
-        <p class="text-[60px] font-[550] text-gray-500">- &nbsp;</p>
-        <p class="text-[60px] font-extralight italic text-gray-500 antialiased">Naim Süleymanoğlu</p>
-        <p class="text-[60px] font-[550] text-gray-500">&nbsp; -</p>
+        <p class="absolute text-[60px] font-extralight italic antialiased mb-[4.9%]">
+          Naim Süleymanoğlu
+        </p>
+        <hr class="mt-[0.6%] min-w-[40%] rounded-lg border-[6px] border-gray-500" />
         <p class="text-[60px] font-[550]">></p>
       </section>
 
