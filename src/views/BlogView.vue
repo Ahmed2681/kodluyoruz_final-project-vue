cff5f65f22c1abf4944b22e9ff20b79f1c611a6d
Ahmed <102857181+Ahmed2681@users.noreply.github.com>
2023-03-11 23:41:58 +0300

    mmm

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -12,6 +12,9 @@ import FooterPage from '@/components/FooterPages.vue'
     <main
       class="flex w-full flex-col items-center justify-center bg-cyan-50 pt-[5%] pb-[5%] text-center"
     >
+      <article class="mb-[4%] flex items-center justify-center text-center">
+        <img src="../img/Naim.webp" class="w-[40%] rounded-3xl" alt="Naim-Suleymanoglu" />
+      </article>
       <section class="mt-[3%] flex flex-row items-center justify-center text-center">
         <p class="text-[60px] font-[550]">&lt;</p>
         <p
@@ -32,6 +35,62 @@ import FooterPage from '@/components/FooterPages.vue'
         Birçok otoriteye göre tüm zamanların en iyi haltercisi olarak kabul edilir. Yapıca ufak
         tefek ancak çok güçlü olması nedeniyle Cep Herkül'ü olarak anılan Naim Süleymanoğlu, Türk
         Süpermen adıyla da anılır.
+      </article>
+
+      <br />
+
+      <article
+        class="flex w-[60%] items-center justify-center text-center font-mono text-2xl antialiased"
+      >
+        Haltere 1977'de, 10 yaşındayken başladı. 15 yaşındayken Brezilya'da düzenlenen Dünya Gençler
+        Halter Şampiyonası'nda iki altın madalya alarak şampiyon oldu. 16 yaşında rekor kırarak yine
+        şampiyon oldu. Böylece halter tarihinde en genç dünya rekortmeni unvanını aldı. Kariyeri
+        boyunca 3 altın olimpiyat madalyası, 7 dünya şampiyonluğu ve 6 Avrupa şampiyonluğu vardır.
+        Tam 46 kere dünya rekoru kırmıştır. 1984 yılında (16 yaşındayken), silkme kategorisinde
+        vücut ağırlığının üç katını kaldıran ikinci halterci olarak tarihe geçti. 1983-1986 arasında
+        gençlerde 13, büyüklerde 50 olmak üzere tam 63 rekor kırarken yine bu dönemde dünya ve
+        Avrupa şampiyonalarında 52, 56 ve 60 kilolarda şampiyonluklar yaşadı. 1984, 1985 ve 1986'da
+        dünyada yılın haltercisi seçildi. <br />
+        <br />
+        1984 Los Angeles Olimpiyatları'na Bulgaristan'ın da Sovyetlerle boykotta yer alması
+        nedeniyle katılamadı. Bu dönemde Bulgar Hükûmetinin Soya Dönüş Operasyonu kapsamında Türk
+        isimlerini yasaklaması nedeniyle adı Naum Şalamanov olarak değiştirildi. Bulgaristan'daki bu
+        baskılardan kurtulmak ve Türkiye adına müsabakalara katılmak için 1986'da Melbourne'de
+        düzenlenen Dünya Halter Şampiyonası'nda Türkiye büyükelçiliğine sığınarak Türkiye'ye iltica
+        etti. Türkiye'ye ilticasında ve getirilmesinde bizzat Turgut Özal devreye girdi. Naim
+        Süleymanoğlu 18 Kasım 2017 günü tedavi gördüğü hastanede 50 yaşında hayatını kaybetti.
+      </article>
+
+      <hr
+        class="mt-[10%] mb-[10%] w-[90%] border-[10px] border-dotted border-black border-b-transparent border-l-transparent border-r-transparent"
+      />
+
+      <article class="mb-[4%] flex items-center justify-center text-center">
+        <img src="../img/Muhammed-Ali.jpeg" class="w-[40%] rounded-3xl" alt="Muhammed-Ali" />
+      </article>
+
+      <section class="flex flex-row items-center justify-center text-center">
+        <p class="text-[60px] font-[550]">&lt;</p>
+        <p
+          class="mt-[2%] text-[20px] font-bold italic text-gray-500 antialiased md:absolute md:mt-0 md:mb-[12%] md:text-[60px] md:font-extralight md:text-black lg:mb-[9%]"
+        >
+          Muhammed Ali
+        </p>
+        <hr
+          class="invisible absolute mt-[0.8%] rounded-full border-[6px] border-gray-500 md:visible md:static md:min-w-[520px] lg:mt-[0.6%] lg:min-w-[600px] 2xl:mt-[0.4%]"
+        />
+        <p class="text-[60px] font-[550]">></p>
+      </section>
+
+      <article
+        class="mt-[4%] flex w-[60%] items-center justify-center text-center font-mono text-2xl antialiased"
+      >
+        Muhammed Ali (doğum adıyla Cassius Marcellus Clay Jr.; d. 17 Ocak 1942, Louisville, Kentucky
+        – ö. 3 Haziran 2016, Phoenix, Arizona), Amerikalı profesyonel boksördür. Tüm zamanların en
+        iyi boksörü olarak kabul edilen Muhammed Ali, 20. yüzyılın en önemli spor figürlerinden
+        biridir. "The Greatest" (En Büyük) lakaplı Ali, kariyeri boyunca yaptığı 61 maçın yalnızca 5
+        tanesini kaybetmiştir.[2][3] 1999 yılında Sports Illustrated tarafından "Yüzyılın Sporcusu"
+        ve BBC tarafından "Yüzyılın Spor Kişiliği" seçilmiştir.
       </article>
 
       <br />
